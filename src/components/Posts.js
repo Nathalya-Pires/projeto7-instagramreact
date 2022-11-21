8758f9a90f10cbc2bd5f731c7dc1271f2deb156e
Nathalya-Pires <111201469+Nathalya-Pires@users.noreply.github.com>
2022-11-21 03:13:51 -0400

    dados dinamicos

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,26 @@
+import { useState } from "react"
+
 export default function Posts() {
 
   const dados = [
-    { perfil: "./assets/story2.png", nome: "meowed", post: "./assets/feed1.png", curtiu: "./assets/story6.png", curtidas: "Curtido por respondeai e outras 101.523 pessoas" },
-    { perfil: "./assets/story3.png", nome: "barked", post: "./assets/feed2.png", curtiu: "./assets/sd4.png", curtidas: "Curtido por adorable_animals e outras 99.159 pessoas" },
-    { perfil: "./assets/story2.png", nome: "meowed", post: "./assets/feed3.jpg", curtiu: "./assets/story6.png", curtidas: "Curtido por lukaspg e outras 200.545 pessoas" },
+    { perfil: "./assets/story2.png", nome: "meowed", post: "./assets/feed1.png", curtiu: "./assets/story6.png", por: "respondeai", curtidas: " 101523" },
+    { perfil: "./assets/story3.png", nome: "barked", post: "./assets/feed2.png", curtiu: "./assets/sd4.png", por: "adorable_animals", curtidas: " 99159" },
+    { perfil: "./assets/story2.png", nome: "meowed", post: "./assets/feed3.jpg", curtiu: "./assets/story6.png", por: "lukaspg", curtidas: " 200545" },
   ]
 
   return (
     <div class="posts">
-      {dados.map((p) => <Post key={p.index} perfil={p.perfil} nome={p.nome} post={p.post} curtiu={p.curtiu} curtidas={p.curtidas} />)}
+      {dados.map((p) => <Post key={p.index} perfil={p.perfil} nome={p.nome} post={p.post} curtiu={p.curtiu} por={p.por} curtidas={p.curtidas} />)}
     </div>
   )
 
 }
 
 function Post(props) {
+
+  const [curtiu, setCurtiu] = useState("heart-outline")
+  const [salvar, setSalvar] = useState("bookmark-outline")
+
   return (
     <div class="post">
       <div class="cabeçalho">
@@ -27,23 +33,23 @@ function Post(props) {
         </div>
       </div>
       <div class="foto-post">
-        <img src={props.post} />
+        <img onClick={ () => curtiu === "heart-outline" ? setCurtiu("heart") : ""} src={props.post} />
       </div>
       <div class="rodape">
         <div class="cx-1">
           <div class="r-esquerda">
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon onClick={ () => curtiu === "heart-outline" ? setCurtiu("heart") : setCurtiu("heart-outline")} name={curtiu}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div class="r-direita">
-            <ion-icon name="bookmark-outline"></ion-icon>
+            <ion-icon onClick={ () => salvar === "bookmark-outline" ? setSalvar("bookmark") : setSalvar("bookmark-outline")} name={salvar}></ion-icon>
           </div>
         </div>
         <div class="cx-2">
           <div class="img-txt">
             <img src={props.curtiu} />
-            <p>{props.curtidas}</p>
+            <p>Curtido por <strong>{props.por}</strong> e <strong>outras {curtiu === "heart" ? Number(props.curtidas) + 1 : (props.curtidas) }</strong> pessoas </p>
           </div>
         </div>
       </div>
@@ -51,6 +57,4 @@ function Post(props) {
   )
 
 }
-
-
 
